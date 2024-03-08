d43516802fb1f6831da9a61ac9cdb7179fbd56a8
Ian Ortiz <ian.marcos.ortiz@gmail.com>
2024-03-08 11:23:45 -0500

    chore: project created

---
+++ src/main/java/com/ianmarcos/flowingims/FlowingImsApplication.java
@@ -0,0 +1,13 @@
+package com.ianmarcos.flowingims;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowingImsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlowingImsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ianmarcos/flowingims/FlowingImsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ianmarcos.flowingims;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowingImsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
